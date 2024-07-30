ff057879e4c91e17a6e0688cef9c33b667f34b76
Luis <luisffarias@ufmg.br>
2024-07-30 16:44:25 -0300

    Implementado GerenciemantoDeProduto.cpp

---
+++ src/Entidades/GerenciamentoDeProduto.cpp
@@ -0,0 +1,37 @@
+#include "../../includes/GerenciamentoDeProduto.h"
+#include <iostream>
+
+using namespace std;
+
+GerenciamentoDeProduto::GerenciamentoDeProduto(Produto& produto_) : produto(produto_) {
+}
+
+void GerenciamentoDeProduto::AdicionarEstoque(const string& produto_, int quantidade_) {
+    if (produto.ProdutoPertence(produto_)) {
+        estoque_[produto_] += quantidade_;
+        if(quantidade_ == 1){
+            cout << "Adicionado " << quantidade_ << " unidade de " << produto_ << " ao estoque." << endl;
+        } else {
+            cout << "Adicionado " << quantidade_ << " unidades de " << produto_ << " ao estoque." << endl;
+        }
+    } else {
+        cout << "Produto " << produto_ << " não encontrado." << endl;
+    }
+}
+
+void GerenciamentoDeProduto::RemoverEstoque(const string& produto_, int quantidade_) {
+    if (produto.ProdutoPertence(produto_)) {
+        if (estoque_[produto_] >= quantidade_) {
+            estoque_[produto_] -= quantidade_;
+            if(quantidade_ == 1){
+                cout << "Removido " << quantidade_ << " unidade de " << produto_ << " do estoque."  << endl;
+            } else {
+                cout << "Removido " << quantidade_ << " unidades de " << produto_ << " do estoque."  << endl; 
+                }
+        } else {
+            cout << "Quantidade insuficiente em estoque para " << produto_ << "."  << endl;
+        }
+    } else {
+        cout << "Produto " << produto_ << " não encontrado." << endl;
+    }
+}
