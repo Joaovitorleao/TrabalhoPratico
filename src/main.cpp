2fb37480fb60ef2bf59919552ff51570aa0fee21
João Vitor Araújo Leão <joaovitorleao23@gmail.com>
2024-08-03 16:28:20 -0300

    Arquivo main.cpp (1)

---
+++ src/main.cpp
@@ -0,0 +1,93 @@
+//#include "../includes/Busca.h" DESCOMENTAR QUANDO IMPLEMENTAR BUSCA
+#include "../includes/GerenciamentoDeProduto.h"
+#include "../includes/Produto.h"
+//#include "../includes/RelatorioVenda.h" DESCOMENTAR QUANDO IMPLEMENTAR RELATORIOVENDA
+#include "../includes/Transacoes.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Menu() {
+    cout << "====== Menu de gerenciamento de estoque ======" << endl;
+    cout << "1. Adicionar produto" << endl;
+    cout << "2. Editar produto" << endl;
+    cout << "3. Excluir produto" << endl;
+    cout << "4. Mostrar todos os produtos" << endl;
+    cout << "5. Histórico de transações" << endl;
+    cout << "6. Histórico de produtos" << endl;
+    cout << "7. Sair" << endl;
+    cout << endl << "Escolha uma opção: " << endl;
+}
+
+int main(){
+    Produto produto;
+    GerenciamentoDeProduto gerenciamento(produto);
+    HistoricoDeTransacao historico;
+    int opcao;
+
+    do {
+        Menu();
+        cin >> opcao;
+
+        switch(opcao){
+            case 1: {
+                string nome;
+                float valorCompra, valorVenda;
+                cout << "Digite o nome do produto: " << endl;
+                getline(cin, nome);
+                cout << "Digite o valor de compra do produto: " << endl;
+                cin >> valorCompra;
+                cout << "Digite o valor de venda do produto: " << endl;
+                cin >> valorVenda;
+                produto.AdicionarProduto(nome, valorCompra, valorVenda);
+                break;
+            }
+            case 2: {
+                string id, novoNome;
+                float novoValorCompra, novoValorVenda;
+                cout << "Digite o id do produto a ser editado: " << endl;
+                cin >> id;
+                cout << "Digite o novo nome do produto: " << endl;
+                cin >> novoNome;
+                cout << "Digite o novo valor de compra do produto: " << endl;
+                cin >> novoValorCompra;
+                cout << "Digite o novo valor de venda do produto: " << endl;
+                cin >> novoValorVenda;
+                produto.EditarProduto(id, novoNome, novoValorCompra, novoValorVenda);
+                break;
+            }
+            case 3: {
+                string id;
+                int quantidade;
+                cout << "Digite o valor do id do produto a ser excluido: " << endl;
+                cin >> id;
+                produto.ExcluirProduto(id);
+                break;
+            }
+            case 4: {
+                produto.MostraProdutos();
+                break;
+            }
+            case 5: {
+                historico.imprimirHistorico();
+                break;
+            }
+            case 6: {
+                historico.imprimirHistoricoIndividual();
+                break;
+            }
+            case 7: {
+                cout << "Saindo do programa. Obrigado por utilizar nosso sistema!" << endl;
+                break; 
+            }
+            default: {
+                cout << "Opção inválida. Tente novamente" << endl;
+                break;
+            }
+        }
+    } while(opcao != 7);
+
+    return 0;
+}
