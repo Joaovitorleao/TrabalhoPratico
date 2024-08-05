7e7f050125c24292e110158eed0af44ae58d61cb
vinirod22 <vinicius15201177@gmail.com>
2024-08-05 19:11:32 -0300

    Update HistoricoDeTransacao.cpp

--- src/Entidades/HistoricoDeTransacao.cpp
+++ src/Entidades/HistoricoDeTransacao.cpp
@@ -1,19 +1,20 @@
-#include "HistoricoDeTransacao.hpp"
+#include "HistoricoDeTransacao.hpp" 
 #include <iostream>
 
 using namespace std;
 
-Transacao::Transacao(int idProduto, Tipo tipo, int quantidade, double valor, const string& data)
-    : idProduto(idProduto), tipo(tipo), quantidade(quantidade), valor(valor), data(data) {}
+Transacao::Transacao(int idProduto, Tipo tipo, int quantidade, double valorDeCompra, double valorDeVenda, const string& data)
+    : idProduto(idProduto), tipo(tipo), quantidade(quantidade), valorDeCompra(valorDeCompra), valorDeVenda(valorDeVenda), data(data) {}
 
 int Transacao::getIdProduto() const { return idProduto; }
 Transacao::Tipo Transacao::getTipo() const { return tipo; }
 int Transacao::getQuantidade() const { return quantidade; }
-double Transacao::getValor() const { return valor; }
+double Transacao::getValorDeCompra() const { return valorDeCompra; }
+double Transacao::getValorDeVenda() const { return valorDeVenda; }
 string Transacao::getData() const { return data; }
 
-void HistoricoDeTransacao::registrarTransacao(int idProduto, Transacao::Tipo tipo, int quantidade, double valor, const string& data) {
-    historico.emplace_back(idProduto, tipo, quantidade, valor, data);
+void HistoricoDeTransacao::registrarTransacao(int idProduto, Transacao::Tipo tipo, int quantidade, double valorDeCompra, double valorDeVenda, const string& data) {
+    historico.emplace_back(idProduto, tipo, quantidade, valorDeCompra, valorDeVenda, data);
 }
 
 void HistoricoDeTransacao::mostrarHistorico() const {
@@ -21,7 +22,11 @@ void HistoricoDeTransacao::mostrarHistorico() const {
         cout << "ID Produto: " << transacao.getIdProduto() << endl;
         cout << "Tipo: " << (transacao.getTipo() == Transacao::ENTRADA ? "Entrada" : "Saida") << endl;
         cout << "Quantidade: " << transacao.getQuantidade() << endl;
-        cout << "Valor: " << transacao.getValor() << endl;
+           if (transacao.getTipo() == Transacao::ENTRADA) {
+            cout << "Valor de Compra: " << transacao.getValorDeCompra() << endl;
+        } else {
+            cout << "Valor de Venda: " << transacao.getValorDeVenda() << endl;
+        }
         cout << "Data: " << transacao.getData() << endl;
     }
 }
@@ -32,7 +37,11 @@ void HistoricoDeTransacao::mostrarHistoricoProduto(int idProduto) const {
             cout << "ID Produto: " << transacao.getIdProduto() << endl;
             cout << "Tipo: " << (transacao.getTipo() == Transacao::ENTRADA ? "Entrada" : "Saida") << endl;
             cout << "Quantidade: " << transacao.getQuantidade() << endl;
-            cout << "Valor: " << transacao.getValor() << endl;
+              if (transacao.getTipo() == Transacao::ENTRADA) {
+            cout << "Valor de Compra: " << transacao.getValorDeCompra() << endl;
+        } else {
+            cout << "Valor de Venda: " << transacao.getValorDeVenda() << endl;
+        }
             cout << "Data: " << transacao.getData() << endl;
         }
     }
