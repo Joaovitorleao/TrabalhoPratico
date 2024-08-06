69fbc4f9f7a90676e389618795f8df6053b0135e
Luis <luisffarias@ufmg.br>
2024-08-06 13:45:49 -0300

    Adicionado funções que contabilizam o total de entradas e saídas de um produto

--- includes/GerenciamentoDeProduto.h
+++ includes/GerenciamentoDeProduto.h
@@ -17,9 +17,17 @@ class GerenciamentoDeProduto {
         // Remove uma quantidade do estoque do produto especificado
         void RemoverEstoque(const string& produto_, int quantidade_);
 
+        // Contabiliza o total de entradas de um produto
+        int TotalEntradas(const string& produto_);
+
+        // Contabiliza o total de saídas de um produto
+        int TotalSaidas(const string& produto_);
+
     private:
         Produto& produto;
         map<string, int> estoque_; // Mapeia o nome do produto à quantidade em estoque
+        map<string, int> entradas_; // Mapeia o nome do produto à quantidade de entradas
+        map<string, int> saidas_; // Mapeia o nome do produto à quantidade de saídas
 };
 
 #endif
