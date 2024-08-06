4a107dc7b530f14be880cda9482aa6965f08e17c
Luis <luisffarias@ufmg.br>
2024-08-06 20:57:57 -0300

    Correção de erros

--- includes/GerenciamentoDeProduto.h
+++ includes/GerenciamentoDeProduto.h
@@ -4,16 +4,16 @@
 #include "Produto.h"
 #include <map>
 
-class GerenciamentoDeProduto {
+class GerenciamentoDeProduto : public Produto {
     public:
 
         GerenciamentoDeProduto(Produto& produto);
 
         // Adiciona uma quantidade ao estoque do produto especificado
-        void AdicionarEstoque();
+        void AdicionarEstoque(const string& nome, int quantidade);
 
         // Remove uma quantidade do estoque do produto especificado
-        void RemoverEstoque();
+        void RemoverEstoque(const string& nome, int quantidade);
 
         // Contabiliza o total de entradas de um produto
         int TotalEntradas(const string& produto_);
@@ -21,10 +21,12 @@ class GerenciamentoDeProduto {
         // Contabiliza o total de saídas de um produto
         int TotalSaidas(const string& produto_);
 
-    private:
-        Produto& produto;
         map<string, int> entradas_; // Mapeia o nome do produto à quantidade de entradas
         map<string, int> saidas_; // Mapeia o nome do produto à quantidade de saídas
+
+    private:
+        Produto& produto;
+
 };
 
 #endif
